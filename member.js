6b1c613771e38ae86cea42ffc0c7184c679fd8cb
innervoice1 <vipul.gnu@gmail.com>
2024-07-26 12:31:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  // 1. Create a new object
+  var member = {};
+
+  // 2. Add properties to the object
+  member.name = "John";
+  member.age = 30;
+  member.skills = ["JavaScript", "React", "Node.js"];
+
+  // 3. Return the object
+  return member;
+}
